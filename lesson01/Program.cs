c0ffbc0e3b3213c6328e3135dcdb60483ba8be0e
Igor <greyjedi2@gmail.com>
2022-08-31 00:36:17 +0600

    Add task #3

--- lesson01/Program.cs
+++ lesson01/Program.cs
@@ -6,6 +6,12 @@ Console.WriteLine("Введите второе число");
 string input2 = Console.ReadLine();
 int firstNumber = int.Parse(input1);
 int secondNumber = int.Parse(input2);
-if (firstNumber == secondNumber * secondNumber) Console.WriteLine($"Число {firstNumber} является квадратом {secondNumber}");
-else Console.WriteLine($"Число {firstNumber} НЕ является квадратом {secondNumber}");
+if (firstNumber == secondNumber * secondNumber)
+{
+    Console.WriteLine($"Число {firstNumber} является квадратом {secondNumber}");
+    //Console.WriteLine("Число " + firstNumber + " является квадратом " + secondNumber);
+    //Console.WriteLine("Число {0} является квадратом {1}", firstNumber, secondNumber);
+}
+else
+    Console.WriteLine($"Число {firstNumber} НЕ является квадратом {secondNumber}");
 
