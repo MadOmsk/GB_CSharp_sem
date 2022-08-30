bab4c1e97274efe653d204486782e9897e34ffc8
Igor <greyjedi2@gmail.com>
2022-08-31 00:36:24 +0600

    Add task #3

---
+++ lesson02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+Console.WriteLine("Введите номер дня недели");
+string input = Console.ReadLine();
+int numberOfDay = int.Parse(input);
+
+switch (numberOfDay)
+{
+    case 1:
+        Console.WriteLine("Это понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Это вторник");
+        break;
+    case 3:
+        Console.WriteLine("Это среда");
+        break;
+    case 4:
+        Console.WriteLine("Это четверг");
+        break;
+    case 5:
+        Console.WriteLine("Это пятница");
+        break;
+    case 6:
+        Console.WriteLine("Это суббота");
+        break;
+    case 7:
+        Console.WriteLine("Это воскресенье");
+        break;
+    default:
+        Console.WriteLine("Нет такого дня недели!");
+        break;
+}
