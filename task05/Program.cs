8f0be7da57b442c934ee5514707cf46f806c1761
Igor <greyjedi2@gmail.com>
2022-08-31 01:04:51 +0600

    Add task #5 and #7

---
+++ task05/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Console.WriteLine("Введите число");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+for (int i = -number; i < number; i++)
+{
+    Console.Write(i + ", ");
+}
+Console.Write(number);
