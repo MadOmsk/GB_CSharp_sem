c8fd292b33e54db13dca16503713db6ce3e59fca
Igor <greyjedi2@gmail.com>
2022-08-31 22:16:01 +0600

    Add task #2 and #4

---
+++ task02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.WriteLine("Введите первое число");
+var firstNumber = int.Parse(Console.ReadLine()); //ввод первого числа, преобразование в int
+Console.WriteLine("Введите второе число");
+var secondNumber = int.Parse(Console.ReadLine()); //ввод второго числа, преобразование в int
+
+if (firstNumber > secondNumber)
+    System.Console.WriteLine(firstNumber + " > " + secondNumber);
+else if (firstNumber < secondNumber)
+    System.Console.WriteLine(firstNumber + " < " + secondNumber);
+else
+    System.Console.WriteLine(firstNumber + " = " + secondNumber);
