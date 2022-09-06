6c31d079d79fb6c0c3af79249afdbb20a3489b89
Igor <greyjedi2@gmail.com>
2022-09-06 22:13:02 +0600

    Delete achive

--- Archive (for delete)/Task01/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿int firstNumber = EducationLibrary.ReadLineWithMessage("Введите первое число");
-int secondNumber = EducationLibrary.ReadLineWithMessage("Введите второе число");
-
-if (firstNumber == secondNumber * secondNumber)
-{
-    Console.WriteLine($"Число {firstNumber} является квадратом {secondNumber}");
-}
-else
-    Console.WriteLine($"Число {firstNumber} НЕ является квадратом {secondNumber}");
