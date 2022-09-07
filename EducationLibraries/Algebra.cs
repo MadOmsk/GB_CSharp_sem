efef0191873eca98a984c6a07e6af77e2d3921b3
Igor <greyjedi2@gmail.com>
2022-09-08 00:31:20 +0600

    Algebra.cs update

--- EducationLibraries/Algebra.cs
+++ EducationLibraries/Algebra.cs
@@ -4,18 +4,19 @@ namespace EducationLibraries;
 public static class Algebra
 {
     // Множества чисел: N - натуральные, Z - целые, R - рациональные.
-    public enum Sets { N, Z, R };
+    public enum Sets { N, Z, Q };
 
     // Точность сравнения чисел.
     private static double precision = 1E-14;
+    private static int precisionLog = (int)Math.Log(precision, 0.1);
 
     // Метод принимает на ввод double и set (член enum Sets - множества чисел) и возвращает принадлежность к этому set числа, сохранённого в double.
     public static bool SetOfNumber(double number, Sets set)
     {
         // Переменные, отображающие, является ли число натуральным, целым, рациональным, соответственно.
-        bool isN = false, isZ = false, isR = true;
+        bool isN = false, isZ = false, isQ = true;
         // Определяет целость числа с заданной точностью.
-        if (Math.Abs((double)(int)number - number) < precision)
+        if (Math.Abs((int)Softering(number) - number) < precision)
         {
             isZ = true;
             // Определяет натуральность числа.
@@ -30,12 +31,39 @@ public static class Algebra
                 return isN;
             case Sets.Z:
                 return isZ;
-            case Sets.R:
-                return isR;
+            case Sets.Q:
+                return isQ;
             default:
                 return false;
         }
     }
 
-    // public static void 
+    // Преобразовывает рациональное число в целое. Если не получается, то бросает исключение.
+    public static int RationalToInteger(double number)
+    {
+        if (SetOfNumber(number, Sets.Z))
+            return (int)Softering(number);
+        else throw new ArithmeticException("Число не является целым");
+    }
+
+    // Преобразовывает целое число в натуральное. Если не получается, то бросает исключение.
+    public static int IntegerToNatural(int number)
+    {
+        if (SetOfNumber(number, Sets.N))
+            return number;
+        else throw new ArithmeticException("Число не является натуральным");
+    }
+
+    // Преобразовывает рациональное число в натуральное. Если не получается, то бросает исключение.
+    public static int RationalToNatural(double number)
+    {
+        return IntegerToNatural(RationalToInteger(number));
+    }
+
+    // Отбрасывает погрешность, которая может образоваться при математических операциях с double.
+    public static double Softering(double number)
+    {
+        return Math.Round(number, precisionLog, MidpointRounding.AwayFromZero);
+    }
 }
+
--- Tests/Test.cs
+++ Tests/Test.cs
@@ -1,14 +1,25 @@
 ﻿using EducationLibraries;
 
 
-test(Math.Pow(Math.Sqrt(2), 2));
-test(-0.000000001);
-
+test1(Math.Pow(Math.Sqrt(2), 2));
+test1(-0.000000001);
+test1((3 - Math.Pow(Math.Sqrt(2), 2)));
+test1(0);
+test1(-1);
+test1(1);
+test2(-0.9);
+test2(1.000000001);
+test2(Math.Pow(Math.Sqrt(Math.Pow(Math.Sqrt(2), 2)),2));
+test2(3 - Math.Pow(Math.Sqrt(2), 2));
 
 // Проверяет метод Algebra.SetOfNumber на заданном числе
-void test(double a)
+void test1(double a)
 {
-    Console.WriteLine(a + " is R = " + Algebra.SetOfNumber(a, Algebra.Sets.R));
+    Console.WriteLine(a + " is Q = " + Algebra.SetOfNumber(a, Algebra.Sets.Q));
     Console.WriteLine(a + " is Z = " + Algebra.SetOfNumber(a, Algebra.Sets.Z));
     Console.WriteLine(a + " is N = " + Algebra.SetOfNumber(a, Algebra.Sets.N));
+}
+
+void test2(double a){
+    Console.WriteLine(a + " - after softering " + Algebra.Softering(a));
 }
