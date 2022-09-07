0c24708bf39f26532d9db6b64d0ae458c52dbffc
Igor <igorgrey2@gmail.com>
2022-09-07 16:29:44 +0600

    Add method SetOfNumbers, Class Algebra. Add tests project.

--- EducationLibraries/Algebra.cs
+++ EducationLibraries/Algebra.cs
@@ -1,10 +1,41 @@
 namespace EducationLibraries;
-// Математические методы.
+
+// Алгебра
 public static class Algebra
 {
-    // Метод выводит сообщение, читает строку со ввода терминала и преобразовывает её в int.
-    public static bool TypeOfNumber(string input)
+    // Множества чисел: N - натуральные, Z - целые, R - рациональные.
+    public enum Sets { N, Z, R };
+
+    // Точность сравнения чисел.
+    private static double precision = 1E-14;
+
+    // Метод принимает на ввод double и set (член enum Sets - множества чисел) и возвращает принадлежность к этому set числа, сохранённого в double.
+    public static bool SetOfNumber(double number, Sets set)
     {
-        return true;
+        // Переменные, отображающие, является ли число натуральным, целым, рациональным, соответственно.
+        bool isN = false, isZ = false, isR = true;
+        // Определяет целость числа с заданной точностью.
+        if (Math.Abs((double)(int)number - number) < precision)
+        {
+            isZ = true;
+            // Определяет натуральность числа.
+            if (number > 0)
+                isN = true;
+        }
+
+        // Выводит результат в зависимости от заданного множества
+        switch (set)
+        {
+            case Sets.N:
+                return isN;
+            case Sets.Z:
+                return isZ;
+            case Sets.R:
+                return isR;
+            default:
+                return false;
+        }
     }
+
+    // public static void 
 }
---
+++ Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using EducationLibraries;
+
+
+test(Math.Pow(Math.Sqrt(2), 2));
+test(-0.000000001);
+
+
+// Проверяет метод Algebra.SetOfNumber на заданном числе
+void test(double a)
+{
+    Console.WriteLine(a + " is R = " + Algebra.SetOfNumber(a, Algebra.Sets.R));
+    Console.WriteLine(a + " is Z = " + Algebra.SetOfNumber(a, Algebra.Sets.Z));
+    Console.WriteLine(a + " is N = " + Algebra.SetOfNumber(a, Algebra.Sets.N));
+}
